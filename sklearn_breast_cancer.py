933b841b0070ed664f6649bf3013205546c8ca6d
roguet-g <guilhem.roguet@gmail.com>
2022-10-18 23:42:02 +0200

    Convergence Warning

--- sklearn_breast_cancer.py
+++ sklearn_breast_cancer.py
@@ -1,15 +1,23 @@
 from sklearn.datasets import load_breast_cancer
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 cancer_data = load_breast_cancer()
 
 #print(cancer_data.keys())
 #(cancer_data['DESCR'])
 
-# print datapoints and attributes
+# print datapoints and features
 #print(cancer_data['data'].shape)
 
 #loading data into pandas
 df = pd.DataFrame(cancer_data['data'], columns=cancer_data['feature_names'])
 df['target'] = cancer_data['target']
-print(df.head())
+#print(df.head())
+
+#preparing the regression data
+X = df[cancer_data.feature_names].values
+y = df['target'].values
+
+model = LogisticRegression()
+model.fit(X,y)
