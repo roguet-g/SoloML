37b1c5320b812eedb474655d47454453a1da9512
roguet-g <guilhem.roguet@gmail.com>
2022-10-18 23:28:37 +0200

    loading data into pandas

--- sklearn_breast_cancer.py
+++ sklearn_breast_cancer.py
@@ -1,6 +1,15 @@
 from sklearn.datasets import load_breast_cancer
+import pandas as pd
 
 cancer_data = load_breast_cancer()
 
-print(cancer_data.keys())
-print(cancer_data['DESCR'])
+#print(cancer_data.keys())
+#(cancer_data['DESCR'])
+
+# print datapoints and attributes
+#print(cancer_data['data'].shape)
+
+#loading data into pandas
+df = pd.DataFrame(cancer_data['data'], columns=cancer_data['feature_names'])
+df['target'] = cancer_data['target']
+print(df.head())
