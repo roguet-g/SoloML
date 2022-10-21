460b758798d129540f648ce3ce6f15ca5a5f5963
roguet-g <guilhem.roguet@gmail.com>
2022-10-21 19:22:35 +0200

    print confusing matrix

--- sklearn_metrics.py
+++ sklearn_metrics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 
 df = pd.read_csv('./titanic.csv')
 df['male'] = df['Sex'] == 'male'
@@ -15,3 +15,5 @@ print('accuracy : ', accuracy_score(y, y_pred))
 print('precision : ', precision_score(y, y_pred))
 print('recall : ', recall_score(y, y_pred))
 print('f1 score : ', f1_score(y, y_pred))
+
+print(confusion_matrix(y, y_pred))
