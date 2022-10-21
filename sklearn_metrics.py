9e839a1ebe0cc7f828256517ad2479372483c0ac
roguet-g <guilhem.roguet@gmail.com>
2022-10-21 19:08:44 +0200

    metrics print

--- sklearn_metrics.py
+++ sklearn_metrics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 df = pd.read_csv('./titanic.csv')
 df['male'] = df['Sex'] == 'male'
@@ -9,3 +10,8 @@ y = df['Survived'].values
 model = LogisticRegression()
 model.fit(X,y)
 y_pred = model.predict(X)
+
+print('accuracy : ', accuracy_score(y, y_pred))
+print('precision : ', precision_score(y, y_pred))
+print('recall : ', recall_score(y, y_pred))
+print('f1 score : ', f1_score(y, y_pred))
