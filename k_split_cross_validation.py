95501698c0d28fc2086c80d42e2554f26f3d18cc
roguet-g <guilhem.roguet@gmail.com>
2022-10-23 14:35:08 +0200

    kfold looped

--- k_split_cross_validation.py
+++ k_split_cross_validation.py
@@ -1,5 +1,6 @@
 from sklearn.model_selection import KFold
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv('./titanic.csv')
@@ -7,16 +8,14 @@ df['male'] = df['Sex'] == 'male'
 X = df[['Pclass', 'male', 'Age', 'Siblings/Spouses','Parents/Children']].values
 y = df['Survived'].values
 
+scores = []
 kf = KFold(n_splits=5, shuffle=True)
-splits = list(kf.split(X))
 
-train_indices, test_indices = splits[0]
+for train_indices, test_indices in kf.split(X): 
+    X_train, X_test = X[train_indices], X[test_indices]
+    y_train, y_test = y[train_indices], y[test_indices]
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    scores.append(model.score(X_test, y_test))
 
-X_train = X[train_indices]
-X_test = X[test_indices]
-y_train = y[train_indices]
-y_test = y[test_indices]
-
-model = LogisticRegression()
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
+print(np.mean(scores))
