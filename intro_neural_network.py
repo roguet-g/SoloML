9d6df676be165c5b76de8b317f6c257dd9d4ef82
roguet-g <guilhem.roguet@gmail.com>
2022-10-30 18:10:48 +0100

    adam is the best boi

--- intro_neural_network.py
+++ intro_neural_network.py
@@ -6,7 +6,7 @@ X,y = make_classification(n_features=2, n_redundant=0, n_informative=2, random_s
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=3)
 
-mlp = MLPClassifier(max_iter=1000, hidden_layer_sizes=(100, 50), alpha=0.0001, solver='sgd', random_state=3)
+mlp = MLPClassifier(max_iter=1000, hidden_layer_sizes=(100, 50), alpha=0.0001, solver='adam', random_state=3)
 mlp.fit(X_train, y_train)
 
 print('accuracy : ', mlp.score(X_test, y_test))
